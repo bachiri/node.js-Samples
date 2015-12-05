7f8a75a7923fb01e625f16eed4fac944930dc9f3
light <light@MacBook-Pro-de-light.local>
2015-12-05 20:56:09 +0000

    first Sample Not yet finalized

---
+++ simpleServer/server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var url=require('url');
+var path=require('path');
+var fs=require('fs'); 
+
+
+//Array of Mime types 
+var mimeTypes={
+	"html":"text/html",
+	 "jpeg":"text/jpeg",
+	 "jpg":"text/jpeg",
+	 "png":"text/png",
+	 "js":"text/javascript",
+	 "css":"text/css",
+}
+
+//Create Server
